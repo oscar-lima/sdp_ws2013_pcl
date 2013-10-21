bd5921f90eb9ef97ba738f33e31342aee51c6909
Oscar Lima <olima_84@yahoo.com>
2013-10-21 14:11:06 +0200

    Downsampling now is compiling :)

--- downsampling/voxel_grid.cpp
+++ downsampling/voxel_grid.cpp
@@ -5,8 +5,8 @@
 
 int main (int argc, char** argv)
 {
-  pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
-  pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
   // Fill in the cloud data
   pcl::PCDReader reader;
@@ -16,7 +16,8 @@ int main (int argc, char** argv)
   std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height << " data points (" << pcl::getFieldsList (*cloud) << ").";
 
   // Create the filtering object
-  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  pcl::VoxelGrid<pcl::PointXYZ> sor;
+ 
   sor.setInputCloud (cloud);
   sor.setLeafSize (0.01f, 0.01f, 0.01f);
   sor.filter (*cloud_filtered);
@@ -24,7 +25,7 @@ int main (int argc, char** argv)
   std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
 
   pcl::PCDWriter writer;
-  writer.write ("table_scene_lms400_downsampled.pcd", *cloud_filtered, Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+  writer.write ("table_scene_lms400_downsampled.pcd", *cloud_filtered);
 
   return (0);
 }
