d7fdcc4a3d8e0513f32aa6f15718518d79f77012
Oscar Lima <olima_84@yahoo.com>
2013-10-28 14:54:47 +0100

    Changing path to obbey new structure without eclipse

--- examples/downsampling/voxel_grid.cpp
+++ examples/downsampling/voxel_grid.cpp
@@ -3,29 +3,29 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 
-int main (int argc, char** argv)
+int main(int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
 
   // Fill in the cloud data
   pcl::PCDReader reader;
   // Replace the path below with the path where you saved your file
-  reader.read ("table_scene_lms400.pcd", *cloud); // Remember to download the file first!
+  reader.read("table_scene_lms400.pcd", *cloud); // Remember to download the file first!
 
-  std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height << " data points (" << pcl::getFieldsList (*cloud) << ").";
+  std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height << " data points (" << pcl::getFieldsList(*cloud) << ").";
 
   // Create the filtering object
   pcl::VoxelGrid<pcl::PointXYZ> sor;
  
-  sor.setInputCloud (cloud);
+  sor.setInputCloud(cloud);
   sor.setLeafSize (0.01f, 0.01f, 0.01f);
-  sor.filter (*cloud_filtered);
+  sor.filter(*cloud_filtered);
 
-  std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
+  std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height << " data points (" << pcl::getFieldsList(*cloud_filtered) << ").";
 
   pcl::PCDWriter writer;
-  writer.write ("table_scene_lms400_downsampled.pcd", *cloud_filtered);
+  writer.write("table_scene_lms400_downsampled.pcd", *cloud_filtered);
 
-  return (0);
+  return 0;
 }
