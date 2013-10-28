0dfe7e89e8aa4493c74616b3491ddba00d092f1b
Iuri Andrade <iuri.araujo@smail.inf.h-brs.de>
2013-10-28 18:48:26 +0100

    Creating a use case of the program (load file, filter, visualize) - Pair programming

--- pcl_agent/src/pcl_tester.cpp
+++ pcl_agent/src/pcl_tester.cpp
@@ -3,14 +3,13 @@
 //#include <pcl/common/common_headers.h>
 //#include <pcl/features/normal_3d.h>
 //#include <pcl/io/pcd_io.h>
-//#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
 //#include <pcl/console/parse.h>
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-
 #include "pcd_simple_io.h"
-#include "cloud_viewer.h"
+//#include "cloud_viewer.h"
 #include "filters.h"
 
 int main(int argc, char** argv)
@@ -26,35 +25,32 @@ int main(int argc, char** argv)
 	<< "4. Apply passtrough filter\n\n";
   
   cin >> option;
-/*  
+
   cout << "\nSelected option : " << option << "\nEnd of program\n";
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+  pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
   
   switch(option)
   {
-    case 0:	cout << "Not a valid option";
+    case 1:
+      cloud = reader.read("../../data/pcd_files/table_scene_lms400.pcd");
       break;
-    case 1:	CloudViewer viewer(reader.read("../../data/pcd_files/table_scene_lms400.pcd"));
+    case 2:
+      cloud = reader.read("../../data/pcd_files/kitchen.pcd");
       break;
-    case 2:	CloudViewer viewer(reader.read("../../data/pcd_files/kitchen.pcd"));
+    case 3:
+      //TODO get image from kinect image
       break;
-    case 3:	CloudViewer viewer(reader.read("../../data/pcd_files/table_scene_lms400_downsampled.pcd"));
+    default:
+      cout << "Error";
       break;
-    case 4:	cout << "Under construction";
-      break;
-    default:	cout << "Error";
-      break;
-  }
-  */
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = reader.read("../../data/pcd_files/table_scene_lms400.pcd");
-  CloudViewer viewer;
-  Filters filter;
-  filter.filter_Downsampling(cloud, cloud, 0.01f, 0.01f, 0.01f);
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> window = viewer.simpleVis(cloud);
-  while (!window->wasStopped()) {
-    window->spinOnce(100);
-    boost::this_thread::sleep(boost::posix_time::microseconds(100000));
   }
   
+  while(!viewer.wasStopped()){
+    viewer.showCloud(cloud);
+    sleep(3);
+    Filters::filter_Downsampling(cloud, cloud, 0.01f, 0.01f, 0.01f);
+  }
   //reader.write();
   return 0;
 }
