c6d74258fc675ecea76006ad994729379f71b610
shehzad <shehzi001@gmail.com>
2013-11-02 00:31:16 +0100

    Problem solved:downsampling switch case

--- pcl_agent/src/pcl_tester.cpp
+++ pcl_agent/src/pcl_tester.cpp
@@ -1,3 +1,8 @@
+/*.......................................................
+ * Authors: Shehzad Ahmed,Oscar Lima,Iuri Andrade
+ * Project:PCL Agentification
+*/
+
 //Class test section, (uncomment to test)
 //#include <boost/thread/thread.hpp>
 //#include <pcl/common/common_headers.h>
@@ -18,69 +23,82 @@ string pcd_example = "table.pcd";
 string pcd_data_folder = "../../data/pcd_files/";
 int sleep_for_x_sec = 0.5;	//helps to do less times the while cicle
 
-static void visualize_pcd(point_cloud cloud);
-static string ask_for_file_name();
-static point_cloud cloud_to_apply_filter();
+
+void menu(void);
+void visualize_pcd(point_cloud cloud);
+string ask_for_file_name();
+point_cloud cloud_to_apply_filter();
 PCDSimpleIO cloud_reader;
 
 int main(int argc, char** argv)
 {
   int option = 0;
   string file_name = pcd_example;
-
-  cout 	<< "\n::Welcome to pointcloud tester::\n\n"
-	<< "Option menu :\n\n"
-	<< "1. Load example\n"
-	<< "2. Load from file\n"
-	<< "3. View from Kinect\n"
-	<< "4. Apply downsampling filter\n"
-	<< "5. Apply passtrough filter\n\n";
-  
-  cin >> option;
-
   point_cloud cloud;
-  //pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+  pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
   
-  switch(option)
+  
+  cout<< "\n::Welcome to pointcloud tester::\n\n";
+  
+  while(!viewer.wasStopped ())
   {
-    case 1:
-			//cloud = reader.read("../../data/pcd_files/table_scene_lms400.pcd");
-			visualize_pcd(cloud_reader.get_cloud(pcd_data_folder + pcd_example));
-			break;
-    case 2:
-			//cloud = reader.read("../../data/pcd_files/kitchen.pcd");
-			{
-				file_name = ask_for_file_name();
-				visualize_pcd(cloud_reader.get_cloud(pcd_data_folder + file_name));
-			} break;
-		
-    case 3:
-			{
-				SimpleOpenNIViewer kinect;
-				kinect.run();
-			}
-	break;
-		
-	case 4:		//Downsample
-			{
-				cloud = cloud_to_apply_filter();
-				Filters::downsampling(cloud, cloud, 0.01f, 0.01f, 0.01f);
-				visualize_pcd(cloud);
-			} break;
-	
-	case 5:		//Apply passtrough filter
-			{
-				cout << "Under construction";
-			} break;
-	
-	case 6:		//exit
-			{
+	  menu();
+	  cin >> option;
+	  switch(option)
+	  { 
+		case 1:
+				cloud = cloud_reader.get_cloud("../../data/pcd_files/table.pcd");
+				//visualize_pcd(cloud);
+				viewer.showCloud(cloud);		
+				break;
+		case 2:
+				//cloud = reader.read("../../data/pcd_files/kitchen.pcd");
+				{
+					//file_name = ask_for_file_name();
+					cloud = cloud_reader.get_cloud(pcd_data_folder + ask_for_file_name());
+					viewer.showCloud(cloud);
+				} 
+		break;
 			
-			} break;
-    default:
-			cout << "\n\nInvalid option\n\n";
-			break;
+		case 3:
+				{
+					SimpleOpenNIViewer kinect;
+					kinect.run();
+				}
+		break;
+			
+		case 4:		//Downsample
+				{		
+					if(cloud == 0)
+						cloud = cloud_reader.get_cloud(pcd_data_folder + ask_for_file_name());
+						
+					Filters::downsampling(cloud, cloud, 0.01f, 0.01f, 0.01f);
+					//visualize_pcd(cloud);
+					viewer.showCloud(cloud);
+					cout << "\n Cloud filtered.. \n";
+				} 
+		break;
+		
+		case 5:		//Apply passtrough filter
+				{
+					cout << "Under construction";
+					
+				} 
+		break;
+		
+		case 6:		//exit
+				{
+					cout << "\n\nEnd of program";
+					viewer.~CloudViewer();
+					return 0;
+				} 
+		break;
+		default:
+				cout << "\n\nInvalid option\n\n";
+				break;
   }
+  option = 0 ;
+}
   /*
   while(!viewer.wasStopped()){
     viewer.showCloud(cloud);
@@ -88,11 +106,12 @@ int main(int argc, char** argv)
     Filters::downsampling(cloud, cloud, 0.01f, 0.01f, 0.01f);
   }*/
   //reader.write();
+  
   cout << "End of program";
   return 0;
 }
 
-static void visualize_pcd(point_cloud cloud)
+void visualize_pcd(point_cloud cloud)
 {
 	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
 	viewer.showCloud(cloud);
@@ -100,20 +119,26 @@ static void visualize_pcd(point_cloud cloud)
 	while(!viewer.wasStopped()) sleep(sleep_for_x_sec);
 }
 
-static string ask_for_file_name()
+string ask_for_file_name()
 {
-	string file_name = pcd_example;
-	
+	string file_name;
 	cout << "\nPlease enter the file name : ";
 	cin >> file_name;
-	
 	return file_name;
 }
 
-static point_cloud cloud_to_apply_filter()
+point_cloud cloud_to_apply_filter()
 {
-	point_cloud cloud;
-	
-	cloud_reader.get_cloud(pcd_data_folder + ask_for_file_name());
-	return cloud;
+	return cloud_reader.get_cloud(pcd_data_folder + ask_for_file_name());
+}
+
+void menu(void){
+
+	cout << "Option menu :\n\n"
+	<< "1. Load example\n"
+	<< "2. Load from file\n"
+	<< "3. View from Kinect\n"
+	<< "4. Apply downsampling filter\n"
+	<< "5. Apply passtrough filter\n"
+	<< "6. Exit.\n\n";
 }
