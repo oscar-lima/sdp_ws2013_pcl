bbe56173f02cfa1afc7aedab62f4b030000f07a3
Iuri Andrade <iuri.araujo@smail.inf.h-brs.de>
2013-12-01 19:35:28 +0100

    Added displayer.cpp .h agent

---
+++ ros_agents/pcl_viewer/include/displayer.h
@@ -0,0 +1,30 @@
+/*
+ * displayer.h
+ *
+ *  Created on: Dec 1, 2013
+ *      Author: Iuri Andrade
+ */
+
+#ifndef DISPLAYER_H_
+#define DISPLAYER_H_
+
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ> CloudXYZ;
+
+class Displayer {
+public:
+	//void setParams(bool colored);
+	void Displayer();
+	void setDisplayedCloud(CloudXYZ::Ptr cloud);
+	CloudXYZ::Ptr getDisplayedCloud();
+	
+private:
+	//bool colored = false;
+	CloudXYZ::Ptr displayedCloud;
+	pcl::visualization::CloudViewer viewer;
+	
+};
+
+#endif /* DISPLAYER_H_ */
---
+++ ros_agents/pcl_viewer/src/displayer.cpp
@@ -0,0 +1,22 @@
+/*
+ * displayer.cpp
+ *
+ *  Created on: Dec 1, 2013
+ *      Author: Iuri Andrade
+ */
+
+#include "displayer.h"
+
+void Displayer::Displayer(){
+	Displayer::viewer = new CloudViewer("Cloud Viewer");
+}
+
+void Displayer::setDisplayedCloud(CloudXYZ::Ptr cloud){
+	Displayer::viewer.showCloud(cloud);
+	Displayer::displayedCloud = cloud;
+}
+
+CloudXYZ::Ptr Displayer::getDisplayedCloud(){
+	return Displayer::displayedCloud;
+}
+
