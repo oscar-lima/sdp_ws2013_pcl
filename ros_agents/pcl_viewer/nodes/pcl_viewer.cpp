8b589c72eef271cad1511c5af42fd5d45c5d64b5
Iuri Andrade <iuri.araujo@smail.inf.h-brs.de>
2013-12-01 19:37:59 +0100

    Code refactoring..

--- ros_agents/pcl_viewer/nodes/pcl_viewer.cpp
+++ ros_agents/pcl_viewer/nodes/pcl_viewer.cpp
@@ -3,25 +3,19 @@
 #include "pcl_ros/point_cloud.h"
 #include <pcl/visualization/cloud_viewer.h>
 
-typedef pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud;
-
 pcl::visualization::CloudViewer viewer("Cloud Viewer");
 
-void cloud_cb(const point_cloud cloud)
-{
- 
+void cloud_cb(const point_cloud cloud){
  viewer.showCloud(cloud);
- 
 }
 
-int main (int argc, char** argv)
-{
+int main (int argc, char** argv){
   // Initialize ROS
-  ros::init (argc, argv, "pcl_viewer");
+  ros::init(argc, argv, "pcl_viewer");
   ros::NodeHandle nh;
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("cloud", 1, cloud_cb);
+  ros::Subscriber sub = nh.subscribe("cloud", 1, cloud_cb);
 
   // Spin
-  ros::spin ();
+  ros::spin();
 }
