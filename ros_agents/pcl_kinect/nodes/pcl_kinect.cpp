d3bbbc01b006a95a5ca68dffc83d9d6ed0473960
Iuri Andrade <iuri.araujo@smail.inf.h-brs.de>
2013-12-16 18:20:06 +0100

    fixed compilation problems for pcl_kinect

--- ros_agents/pcl_kinect/nodes/pcl_kinect.cpp
+++ ros_agents/pcl_kinect/nodes/pcl_kinect.cpp
@@ -3,20 +3,20 @@
 #include "pcl_ros/point_cloud.h"
 #include "kinect_reader.h"
 #include <dynamic_reconfigure/server.h>
-#include "pcl_kinect/KinectConfig.h"
+#include "pcl_kinect/kinectConfig.h"
 
 std::string kinect_pub;
 ros::Publisher pub;
 KinectReader *kinect_live;
 
-void reconfigureCallback(pcl_kinect::KinectConfig &config, uint32_t level);
+void reconfigureCallback(pcl_kinect::kinectConfig &config, uint32_t level);
 /*
 void publisherCallback(CloudXYZ::Ptr cloud){
 	viewer->setDisplayedCloud(cloud);
 	//ROS_INFO("Cloud received: %lu points", viewer->getDisplayedCloud()->points.size());
 }*/
 
-void reconfigureCallback(pcl_kinect::KinectConfig &config, uint32_t level){
+void reconfigureCallback(pcl_kinect::kinectConfig &config, uint32_t level){
 	// Create a ROS subscriber for the input point cloud
 	ROS_INFO("Topic name received: %s", config.kinect_pub.c_str());
 	if (!config.kinect_pub.empty() && !(kinect_pub == config.kinect_pub.c_str())){
@@ -32,8 +32,8 @@ int main(int argc, char** argv){
 	// Initialize ROS
 	ros::init(argc, argv, "pcl_kinect");
 	
-	dynamic_reconfigure::Server<pcl_kinect::KinectConfig> server;
-	dynamic_reconfigure::Server<pcl_kinect::KinectConfig>::CallbackType f;
+	dynamic_reconfigure::Server<pcl_kinect::kinectConfig> server;
+	dynamic_reconfigure::Server<pcl_kinect::kinectConfig>::CallbackType f;
 	
 	f = boost::bind(&reconfigureCallback, _1, _2);
 	server.setCallback(f);
