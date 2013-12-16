15e5144f9c62e3c54848e480c4e5415ce3697d9b
Iuri Andrade <iuri.araujo@smail.inf.h-brs.de>
2013-12-16 18:29:50 +0100

    Fixed compilation problems

--- ros_agents/pcl_viewer/nodes/pcl_viewer.cpp
+++ ros_agents/pcl_viewer/nodes/pcl_viewer.cpp
@@ -3,20 +3,20 @@
 #include "pcl_ros/point_cloud.h"
 #include "displayer.h"
 #include <dynamic_reconfigure/server.h>
-#include "pcl_viewer/ViewerConfig.h"
+#include "pcl_viewer/viewerConfig.h"
 
 Displayer *viewer;
 std::string viewer_sub;
 ros::Subscriber subs;
 
-void reconfigureCallback(pcl_viewer::ViewerConfig &config);
+void reconfigureCallback(pcl_viewer::viewerConfig &config);
 
 void viewerCallback(CloudXYZ::Ptr cloud){
 	viewer->setDisplayedCloud(cloud);
 	//ROS_INFO("Cloud received: %lu points", viewer->getDisplayedCloud()->points.size());
 }
 
-void reconfigureCallback(pcl_viewer::ViewerConfig &config, uint32_t level){
+void reconfigureCallback(pcl_viewer::viewerConfig &config, uint32_t level){
 	// Create a ROS subscriber for the input point cloud
 	ROS_INFO("Topic name received: %s", config.viewer_sub.c_str());
 	if (!config.viewer_sub.empty() && !(viewer_sub == config.viewer_sub.c_str())){
@@ -32,8 +32,8 @@ int main(int argc, char** argv){
 	viewer = new Displayer;
 	ros::init(argc, argv, "pcl_viewer");
 	
-	dynamic_reconfigure::Server<pcl_viewer::ViewerConfig> server;
-	dynamic_reconfigure::Server<pcl_viewer::ViewerConfig>::CallbackType f;
+	dynamic_reconfigure::Server<pcl_viewer::viewerConfig> server;
+	dynamic_reconfigure::Server<pcl_viewer::viewerConfig>::CallbackType f;
 	
 	f = boost::bind(&reconfigureCallback, _1, _2);
 	server.setCallback(f);
